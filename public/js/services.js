466512d4edd7d0ec78b98ac03f81623138780345
cristianh6 <cristian.casalaspro@gmail.com>
2023-10-02 13:22:05 -0300

    Service design

--- public/js/services.js
+++ public/js/services.js
@@ -4,39 +4,58 @@ console.log("services success")
 
 arrayCardss.forEach(el=> {
     el.forEach(element => {
-        let p = element.lastElementChild;
-            let img = element.children.item(1);
+        let img = element.children.item(1);
+        let cardText = element.children.item(2).firstElementChild;
+        let p = element.children.item(2).firstElementChild.firstElementChild;
+        let cardBody = element.children.item(2);    
+       
         element.addEventListener("mouseover", e=> {
+            if(e.target) {
+                img.style.opacity = "10%";
+                img.style.transition = "all 3s";
+                img.style.display = "none";
+                element.style.transform = "scaleX(105%)";
+                element.style.transition = "all 2s ease-out";
+                p.classList.replace("d-none", "d-block");
+            }
+        })
+
+         /* element.addEventListener("mouseout", e=> {            
+            if(e.target && p.classList.contains("d-block")) {
+                element.style.position = "relative";
+                p.classList.replace("d-block", "d-none")
+                p.style.transition = "all 2s ease-out"
+                p.style.color = "white";
+                p.style.top = "15%";
+                p.style.textAlign = "left"
+                img.style.opacity = "100%";
+                img.style.transition = "all 2s ease-in";        
+
+            }
+        })   */
+
+        /* element.addEventListener("mouseover", e=> {
             if(e.target && p.classList.contains("d-none")) {
                 element.style.position = "relative";
                 p.classList.replace("d-none", "d-block");
                 p.style.position = "absolute";
                 p.style.transition = "all 2s ease-in"
+                
                 p.style.color = "white";
                 p.style.top = "15%";
                 p.style.textAlign = "left"
                 img.style.opacity = "5%";
-                img.style.transition = "all 2s ease-out";    
-            }
-        })
-
-        element.addEventListener("mouseout", e=> {            
-            if(e.target && p.classList.contains("d-block")) {
-                p.classList.replace("d-block", "d-none")
-                element.style.position = "relative";
-                img.style.opacity = "100%";
-                img.style.transition = "all 2s ease-in";        
-                p.style.transition = "all 2s ease-in"
-                p.style.color = "white";
-                p.style.top = "15%";
-                p.style.textAlign = "left"
- 
+                img.style.transition = "all 1s ease-out";    
+                element.appendChild(button);
+                button.classList.add("btn", "btn-success", "w-75", "mx-auto");
+                button.style.transition = "all 1s ease-in";
                 
                 
-
-
             }
-        })
+        }) */
+
+        
+
         
     })
     
