6bbbccf9758989109eabb4730743afe0069f5c34
cristianh6 <cristian.casalaspro@gmail.com>
2023-10-01 23:41:42 -0300

    Demo home + services prueba 1

--- public/js/services.js
+++ public/js/services.js
@@ -1,5 +1,44 @@
-let c1 = document.getElementById("card1");
-let arrayCardss = [document.querySelectorAll(".card")]
-let p = document.querySelectorAll("p");
+let arrayCardss = [document.querySelectorAll(".card")];
 
 console.log("services success")
+
+arrayCardss.forEach(el=> {
+    el.forEach(element => {
+        let p = element.lastElementChild;
+            let img = element.children.item(1);
+        element.addEventListener("mouseover", e=> {
+            if(e.target && p.classList.contains("d-none")) {
+                element.style.position = "relative";
+                p.classList.replace("d-none", "d-block");
+                p.style.position = "absolute";
+                p.style.transition = "all 2s ease-in"
+                p.style.color = "white";
+                p.style.top = "15%";
+                p.style.textAlign = "left"
+                img.style.opacity = "5%";
+                img.style.transition = "all 2s ease-out";    
+            }
+        })
+
+        element.addEventListener("mouseout", e=> {            
+            if(e.target && p.classList.contains("d-block")) {
+                p.classList.replace("d-block", "d-none")
+                element.style.position = "relative";
+                img.style.opacity = "100%";
+                img.style.transition = "all 2s ease-in";        
+                p.style.transition = "all 2s ease-in"
+                p.style.color = "white";
+                p.style.top = "15%";
+                p.style.textAlign = "left"
+ 
+                
+                
+
+
+            }
+        })
+        
+    })
+    
+
+})
