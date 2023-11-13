f82f67e1d80655758ee56b7794eafc18a2137a59
cristianh6 <cristian.casalaspro@gmail.com>
2023-11-13 13:43:01 -0300

    Actualizacion de services

--- public/js/services.js
+++ public/js/services.js
@@ -112,3 +112,10 @@ let recorrer = function() {
                 
             }
         }) */
+
+        const myModal = document.getElementById('myModal')
+        const myInput = document.getElementById('myInput')
+        
+        myModal.addEventListener('shown.bs.modal', () => {
+          myInput.focus()
+        })
